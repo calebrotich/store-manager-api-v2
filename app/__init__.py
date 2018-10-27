fd609ec638a7b83c9cea77e67a965ea3aeee7ae6
Caleb Rotich <calebrotich10@gmail.com>
2018-10-27 14:16:13 +0300

    [Chore #161457608] Configure blueprints for the application
    Divide application in to auth blueprint and the CRUD blueprint

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+from instance.config import config
+
+jwt = JWTManager()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', default='SdaHv342nx!jknr837bjwd?c,lsajjjhw673hdsbgeh')
+
+    jwt.init_app(app)
+
+    from .api.v2 import endpoint_v2_blueprint as v2_blueprint
+    app.register_blueprint(v2_blueprint, url_prefix='/api/v2')
+
+    from .api.v2 import auth_v2_blueprint as v2_blueprint
+    app.register_blueprint(v2_blueprint, url_prefix='/api/v2/auth')
+
+    return app

501a8ec51e9a8addca213623ca21ec6225cc1182
Caleb Rotich <calebrotich10@gmail.com>
2018-10-28 01:26:55 +0300

    [Chore #161491333] Add tests for the products endpoints
    Test retrieve all products when none is found, test retrieve a specific product when it is not available, test update product, test update product with missing parameter and test delete product.
    Tests fail at this  point.

--- app/tests/v2/test_products.py
+++ app/tests/v2/test_products.py
@@ -154,6 +154,21 @@ class TestProduct(base_test.TestBaseClass):
         self.assertEqual(common_functions.convert_response_to_json(
             response)['products'][0][1], self.PRODUCT['product_name'])
 
+    def test_retrieve_all_products_none_found(self):
+        """Test GET /products - when products exist"""
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
+        response = self.app_test_client.get(
+            '{}/products'.format(self.BASE_URL),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'], "There are no products in the store yet")
+
     def test_retrieve_specific_product(self):
         """Test GET /products/id - when product exist"""
 
@@ -177,3 +192,93 @@ class TestProduct(base_test.TestBaseClass):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(common_functions.convert_response_to_json(
             response)['product'][0][1], self.PRODUCT['product_name'])
+
+    def test_retrieve_specific_product_not_found(self):
+        """Test GET /products/id - when product exist"""
+
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
+        response = self.app_test_client.get(
+            '{}/product/1000'.format(self.BASE_URL),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_product(self):
+        """PUT /product/id - with expected success"""
+
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
+        query = """INSERT INTO products(product_name, product_price, category) 
+        VALUES('Hammer', 300, 'Tools')"""
+        
+        database.insert_to_db(query)
+
+        query = """SELECT product_id FROM products WHERE product_name = 'Hammer'"""
+        product_id = database.select_from_db(query)
+
+        response = self.app_test_client.put('{}/product/{}'.format(
+            self.BASE_URL, product_id[0][0]),
+             json={
+                 'product_name':'Jembe',
+                 'product_price': 400,
+                 'category':'Farm tools'
+             },
+            headers=dict(Authorization=token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['product']['product_name'], "Jembe")
+
+    def test_update_product_missing_parameter(self):
+        """PUT /product/id - with expected success"""
+
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
+        query = """INSERT INTO products(product_name, product_price, category) 
+        VALUES('Hammer', 300, 'Tools')"""
+        
+        database.insert_to_db(query)
+
+        query = """SELECT product_id FROM products WHERE product_name = 'Hammer'"""
+        product_id = database.select_from_db(query)
+
+        response = self.app_test_client.put('{}/product/{}'.format(
+            self.BASE_URL, product_id[0][0]),
+             json={
+                 'product_name':'Jembe',
+                 'category':'Farm tools'
+             },
+            headers=dict(Authorization=token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_delete_product(self):
+        """DELETE /product/id"""
+
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
+        query = """INSERT INTO products(product_name, product_price, category) 
+        VALUES('Hammer', 300, 'Tools')"""  
+        database.insert_to_db(query)
+
+        query = """SELECT product_id FROM products WHERE product_name = 'Hammer'"""
+        product_id = database.select_from_db(query)
+
+        response = self.app_test_client.delete('{}/product/{}'.format(
+            self.BASE_URL, product_id[0][0]),
+            headers=dict(Authorization=token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'], "Product deleted successfully")

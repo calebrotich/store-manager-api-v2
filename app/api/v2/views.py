014eb754e9ed43bbc7271ff38bc3d54c0b141c05
Caleb Rotich <calebrotich10@gmail.com>
2018-10-28 12:53:50 +0300

    [Feature #161530436] Create route for handling category

--- app/api/v2/views.py
+++ app/api/v2/views.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource
 
 from . import endpoint_v2_blueprint, auth_v2_blueprint
 from .resources import (
-    auth, products, saleorders
+    auth, products, saleorders, category
 )
 
 API = Api(endpoint_v2_blueprint)
@@ -15,6 +15,7 @@ API.add_resource(products.Product, '/products')
 API.add_resource(products.SpecificProduct, '/product/<int:product_id>')
 API.add_resource(saleorders.SaleOrder, '/saleorder')
 API.add_resource(saleorders.SpecificSaleOrder, '/saleorder/<int:saleorder_id>')
+API.add_resource(category.ProductCategory, '/category')
 
 AUTH_API.add_resource(auth.SignUp, '/signup')
 AUTH_API.add_resource(auth.Login, '/login')

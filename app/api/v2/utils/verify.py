8f987371c8253410bf8d07c20fb737f732edabd3
Caleb Rotich <calebrotich10@gmail.com>
2018-11-01 22:09:08 +0300

    [Chore #161527130] Ensure blacklisted tokens can not be used
    Make the logout effective by ensuring blacklisted tokens do not work

--- app/api/v2/utils/verify.py
+++ app/api/v2/utils/verify.py
@@ -14,6 +14,12 @@ def verify_tokens():
     if not token:
         abort(make_response(jsonify({
                                 "Message": "You need to login"}), 401))
+
+    query = """SELECT token FROM blacklist WHERE  token = '{}'""".format(token)
+    blacklisted = database.select_from_db(query)
+    if blacklisted:
+        abort(make_response(jsonify({
+                        "Message": "Kindly login again"}), 401))
     try:
         data = jwt.decode(token, os.getenv('JWT_SECRET_KEY', default='SdaHv342nx!jknr837bjwd?c,lsajjjhw673hdsbgeh'))
         return data["email"]
@@ -25,8 +31,8 @@ def verify_tokens():
 
 
 
-def verify_post_product_fields(product_price, product_name, category):
-    if not isinstance(product_price, int):
+def verify_post_product_fields(product_price=None, category=None, inventory=None, min_quantity=None, product_name=None):
+    if not isinstance(product_price, int) and product_price:
         abort(make_response(jsonify(
             message="Product price should be an integer"
         ), 400))
@@ -36,12 +42,22 @@ def verify_post_product_fields(product_price, product_name, category):
             message="Price of the product should be a positive integer above 0."
         ), 400))
 
-    if not isinstance(product_name, str):
+    if not isinstance(inventory, int) and inventory:
         abort(make_response(jsonify(
-            message="Product name should be a string"
+            message="Inventory should be an integer"
         ), 400))
 
-    if not isinstance(category, str):
+    if not isinstance(min_quantity, int) and min_quantity:
         abort(make_response(jsonify(
-            message="Bad request. The Category should be a string"
+            message="Minimum quantity should be an integer"
+        ), 400))
+
+    if not isinstance(category, str) and category:
+        abort(make_response(jsonify(
+            message="Category should be an string referencing the category table"
+        ), 400))
+
+    if not isinstance(product_name, str) and product_name:
+        abort(make_response(jsonify(
+            message="Product name should be a string"
         ), 400))

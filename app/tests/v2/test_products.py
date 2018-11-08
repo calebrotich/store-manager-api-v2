12c733b4322c3e7f86479cc0a482e96ab6ba2f63
Caleb Rotich <calebrotich10@gmail.com>
2018-11-08 12:42:27 +0300

    [Chore #161526123] Add test for products endpoint
    This increases the test coverage for the products endpoints

--- app/tests/v2/test_products.py
+++ app/tests/v2/test_products.py
@@ -97,6 +97,74 @@ class TestProduct(base_test.TestBaseClass):
             response)['message'],
             'Product name should be a string')
 
+    def test_add_new_product_with_product_name_missing(self):
+        """Test POST /products
+
+        with the product name missing
+        """
+        response = self.app_test_client.post('{}/products'.format(
+            self.BASE_URL), json={
+                'product_price': 300,
+                'category':self.category_id, 'inventory': 10, 'min_quantity': 5
+                }, headers=dict(Authorization=self.token),
+                content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'],
+            'Provide a name for the product')
+
+    def test_add_new_product_with_product_price_missing(self):
+        """Test POST /products
+
+        with the product price missing
+        """
+        response = self.app_test_client.post('{}/products'.format(
+            self.BASE_URL), json={
+                'product_name': "Hammer",
+                'category':self.category_id, 'inventory': 10, 'min_quantity': 5
+                }, headers=dict(Authorization=self.token),
+                content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'],
+            'Indicate the price of the product')
+
+    def test_add_new_product_with_category_missing(self):
+        """Test POST /products
+
+        with category missing
+        """
+        response = self.app_test_client.post('{}/products'.format(
+            self.BASE_URL), json={
+                'product_name': "Hammer",
+                'product_price': 300, 'inventory': 10, 'min_quantity': 5
+                }, headers=dict(Authorization=self.token),
+                content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'],
+            'Categorize the product')
+
+    def test_add_new_product_with_min_quantity_missing(self):
+        """Test POST /products
+
+        with min quantity missing
+        """
+        response = self.app_test_client.post('{}/products'.format(
+            self.BASE_URL), json={
+                'product_name': "Hammer",
+                'product_price': 300, 'inventory': 10, 'category': self.category_id
+                }, headers=dict(Authorization=self.token),
+                content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'],
+            'Please define the minimum quantity')
+
     def test_add_new_product_with_category_not_recognized(self):
         """Test POST /products
 
@@ -211,7 +279,7 @@ class TestProduct(base_test.TestBaseClass):
             headers=dict(Authorization=self.token),
             content_type='application/json')
 
-        #self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.status_code, 202)
         self.assertEqual(common_functions.convert_response_to_json(
             response)['message'], "Product updated successfully")
 
@@ -227,7 +295,6 @@ class TestProduct(base_test.TestBaseClass):
         response = self.app_test_client.put('{}/product/{}'.format(
             self.BASE_URL, product_id[0]['product_id']),
              json={
-                 'product_name':'Jembe',
                  'product_price': 300
              },
             headers=dict(Authorization=self.token),
@@ -235,6 +302,46 @@ class TestProduct(base_test.TestBaseClass):
 
         self.assertEqual(response.status_code, 403)
 
+    def test_update_product_not_found(self):
+        """PUT /product/id - with expected success"""
+        response = self.app_test_client.put('{}/product/1'.format(
+            self.BASE_URL),
+             json={
+                'product_price': 45000,
+                'min_quantity': 5,
+                'inventory': 55,
+                'category': self.category_id
+             },
+            headers=dict(Authorization=self.token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'], "Product with id 1 does not exist")
+
+    def test_update_product_category_absent(self):
+        """PUT /product/id - with expected success"""
+        self.app_test_client.post('{}/products'.format(
+            self.BASE_URL), json=self.PRODUCT, headers=dict(Authorization=self.token),
+            content_type='application/json')
+
+        query = """SELECT product_id FROM products WHERE product_name = 'Phone Model 1'"""
+        product_id = database.select_from_db(query)
+
+        response = self.app_test_client.put('{}/product/{}'.format(
+            self.BASE_URL, product_id[0]['product_id']),
+             json={
+            'product_price': 45000,
+            'min_quantity': 5,
+            'inventory': 55,
+            'category': -1
+             },
+            headers=dict(Authorization=self.token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'], "Category with id -1 is not recognized")
 
     def test_delete_product(self):
         """DELETE /product/id"""
@@ -254,6 +361,16 @@ class TestProduct(base_test.TestBaseClass):
         self.assertEqual(common_functions.convert_response_to_json(
             response)['message'], "Product deleted successfully")
 
+    def test_delete_product_not_found(self):
+        """DELETE /product/id"""
+        response = self.app_test_client.delete('{}/product/100'.format(
+            self.BASE_URL),
+            headers=dict(Authorization=self.token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(common_functions.convert_response_to_json(
+            response)['message'], "Product with id 100 does not exist")
 
     def test_non_json_data(self):
         """Test POST /products"""
